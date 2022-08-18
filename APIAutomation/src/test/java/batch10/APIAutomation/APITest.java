f2d64055b4d3e6b3f6a4ad80fa6e4685097d07da
andrazaiin <andrazaiin@gmail.com>
2022-08-18 11:09:50 +0700

    update task API automation

--- APIAutomation/src/test/java/batch10/APIAutomation/APITest.java
+++ APIAutomation/src/test/java/batch10/APIAutomation/APITest.java
@@ -1,32 +1,69 @@
 package batch10.APIAutomation;
 
-import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
 
-import org.testng.AssertJUnit;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 
-public class APITest extends BaseApiTest {
-
+public class APITest {
+	
+	String baseUrl = "https://api-staging-builder.engineer.ai";
+	String token;
+	
+	Faker faker = new Faker();
+	
 	@Test
-	public void signIn() {
-		RestAssured.baseURI = "https://api-staging-builder.engineer.ai";
+	public void signInWithFaker() {
 		
-		String payload = "{\n"
-				+ "  \"email\": \"testlabs@gmail.com\",\n"
-				+ "  \"password\": \"builder123\"\n"
-				+ "}";
+		RestAssured.baseURI = baseUrl;
 		
-//		Response responseLogin = RestAssured.given().contentType("application/json")
-//				.body(payload).when().post("/users/sign_in");
-//		token = responseLogin.jsonPath().get("user.authtoken");
-//		System.out.println(token);
+		JSONObject request = new JSONObject();
 		
-		Response responseUser = given().spec(commonJsonSpec).when().get("/users/sign_in");
-		AssertJUnit.assertEquals(responseUser.statusCode(), 200);
+		request.put("first_name", faker.name().firstName());
+		request.put("last_name", faker.name().lastName());
+		request.put("email", faker.bothify("?????###@yopmail.com"));
+		request.put("password", "builder123");
+		request.put("phone_number", "+62-" + faker.numerify("###########"));
+		request.put("user_type", "User");
+		request.put("currency_id", "2");
+		
+		System.out.println(request.toString());
+		String payload = request.toString();
+		
+		Response responseSignUp = RestAssured.given()
+				.contentType("application/json")
+				.body(payload)
+				.when().post("/users");
+		
+		//pasring json body to string
+		ResponseBody getBodySignUp = responseSignUp.getBody();
+		String resultStringGetBodySignUp = getBodySignUp.asString();
+		
+		//get token
+		token = responseSignUp.jsonPath().get("user.authtoken");
+		
+		//get dummy data
+		String dummyName = responseSignUp.jsonPath().get("user.first_name");
+		String dummyEmail = responseSignUp.jsonPath().get("user.email");
+		String dummyPhoneNumber = responseSignUp.jsonPath().get("user.phone_number");
+		
+		System.out.println(token);
+		System.out.println("****** RESPONSE NEW SIGN UP ****** " + resultStringGetBodySignUp);
+	
+		System.out.println(dummyName);
+		System.out.println(dummyEmail);
+		System.out.println(dummyPhoneNumber);
+		
+		//assert
+		assertEquals(responseSignUp.statusCode(), 200);
 		
 	}
+
 	
 }
